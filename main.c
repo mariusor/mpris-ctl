6213386ccfe55191a9c3c6b2d589ac962aacd026
Marius Orcsik <marius@habarnam.ro>
2017-03-22 17:37:09 +0100

    Some cleanup

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
  */
 
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dbus/dbus.h>
 
-#define DBUS_LOCAL_PLAYER               "org.mpris.mprisctl"
-#define DBUS_MPRIS_DESTINATION            "org.mpris.MediaPlayer2.spotify"
-#define DBUS_MPRIS_PLAYER_PATH          "/org/mpris/MediaPlayer2"
-#define DBUS_MPRIS_PLAYER_INTERFACE     "org.mpris.MediaPlayer2.Player"
-#define DBUS_MPRIS_METHOD_NEXT          "Next"
-#define DBUS_MPRIS_METHOD_PREVIOUS      "Previous"
-#define DBUS_MPRIS_METHOD_PLAY          "Play"
-#define DBUS_MPRIS_METHOD_PAUSE         "Pause"
-#define DBUS_MPRIS_METHOD_STOP          "Stop"
-#define DBUS_MPRIS_METHOD_PLAY_PAUSE    "PlayPause"
+#define LOCAL_NAME               "org.mpris.mprisctl"
+#define MPRIS_DESTINATION          "org.mpris.MediaPlayer2"
+#define MPRIS_PLAYER_PATH          "/org/mpris/MediaPlayer2"
+#define MPRIS_PLAYER_INTERFACE     "org.mpris.MediaPlayer2.Player"
+#define MPRIS_METHOD_NEXT          "Next"
+#define MPRIS_METHOD_PREVIOUS      "Previous"
+#define MPRIS_METHOD_PLAY          "Play"
+#define MPRIS_METHOD_PAUSE         "Pause"
+#define MPRIS_METHOD_STOP          "Stop"
+#define MPRIS_METHOD_PLAY_PAUSE    "PlayPause"
 
 #define ARG_HELP        "help"
 #define ARG_PLAY        "play"
@@ -26,48 +27,46 @@
 #define ARG_PREVIOUS    "prev"
 #define ARG_PLAY_PAUSE  "pp"
 
-char* get_version()
+const char* get_version()
 {
 #ifdef USE_VERSION
-    char* version = VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH;
+    const char* version = VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH;
 #else
-    char* version = "(local-build)";
+    const char* version = "(local-build)";
 #endif
     return version;
 }
 
-char* get_dbus_method (char* command) 
+const char* get_dbus_method (char* command) 
 {
     if (NULL == command) return NULL;
 
-    char* dbus_method;
-
     if (strcmp(command, ARG_PLAY) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_PLAY;
+        return MPRIS_METHOD_PLAY;
     }
     if (strcmp(command,ARG_PAUSE) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_PAUSE;
+        return MPRIS_METHOD_PAUSE;
     }
     if (strcmp(command, ARG_STOP) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_STOP;
+        return MPRIS_METHOD_STOP;
     }
     if (strcmp(command, ARG_NEXT) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_NEXT;
+        return MPRIS_METHOD_NEXT;
     }
     if (strcmp(command, ARG_PREVIOUS) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_PREVIOUS;
+        return MPRIS_METHOD_PREVIOUS;
     }
     if (strcmp(command, ARG_PLAY_PAUSE) == 0) {
-        dbus_method = DBUS_MPRIS_METHOD_PLAY_PAUSE;
+        return MPRIS_METHOD_PLAY_PAUSE;
     }
 
-    return dbus_method;
+    return NULL;
 }
 
 void print_help(char* name)
 {
-    char* help_msg;
-    char* version = get_version();
+    const char* help_msg;
+    const char* version = get_version();
 
     help_msg = "MPRIS player control, version %s\n" \
                "Usage: %s [" ARG_PLAY "|" \
@@ -80,16 +79,37 @@ void print_help(char* name)
     fprintf(stdout, help_msg, version, name);
 }
 
+char* get_player_name(DBusConnection* conn) {
+    char* player_name;
+    if (NULL == conn) { return NULL; }
+
+    
+
+    player_name = ".spotify";
+    char *ret = malloc(strlen(player_name));
+    strcpy(ret, player_name);
+    return ret;
+}
+
+char* get_dbus_destination(DBusConnection* conn) 
+{
+    const char* mpris_namespace = MPRIS_DESTINATION;
+    char* player_name = get_player_name(conn);
+    char* full_name = malloc(strlen(mpris_namespace)+strlen(player_name)+1);
+
+    strcpy(full_name, mpris_namespace);
+    strcat(full_name, player_name);
+
+    return full_name;
+}
+
 DBusPendingCall* call_dbus_method(DBusConnection* conn, char* destination, char* path, char* interface, char* method) 
 {
     DBusMessage* msg;
     DBusPendingCall* pending;
 
     // create a new method call and check for errors
-    msg = dbus_message_new_method_call(destination, // target for the method call
-                                      path, // object to call on
-                                      interface, // interface to call on
-                                      method); // method name
+    msg = dbus_message_new_method_call(destination, path, interface, method);
     if (NULL == msg) { 
         fprintf(stderr, "Message Null!");
         return NULL;
@@ -107,6 +127,7 @@ DBusPendingCall* call_dbus_method(DBusConnection* conn, char* destination, char*
 
     // free message
     dbus_message_unref(msg);
+
     return pending;
 }
 
@@ -125,8 +146,7 @@ int main(int argc, char** argv)
         goto _success;
     }
 
-    char *dbus_method = get_dbus_method(command);
-    //DBusMessageIter args;
+    char *dbus_method = (char*)get_dbus_method(command);
     DBusConnection* conn;
     DBusError err;
 
@@ -143,9 +163,8 @@ int main(int argc, char** argv)
         goto _error;
     }
 
-    int ret;
     // request a name on the bus
-    ret = dbus_bus_request_name(conn, DBUS_LOCAL_PLAYER, 
+    int ret = dbus_bus_request_name(conn, LOCAL_NAME, 
                                DBUS_NAME_FLAG_REPLACE_EXISTING,
                                &err);
     if (dbus_error_is_set(&err)) { 
@@ -157,9 +176,11 @@ int main(int argc, char** argv)
     }
 
     DBusPendingCall* pending;
-    pending = call_dbus_method(conn, DBUS_MPRIS_DESTINATION, // target for the method call
-                           DBUS_MPRIS_PLAYER_PATH, // object to call on
-                           DBUS_MPRIS_PLAYER_INTERFACE, // interface to call on
+    char* destination = get_dbus_destination(conn);
+    
+    pending = call_dbus_method(conn, destination, // target for the method call
+                           MPRIS_PLAYER_PATH, // object to call on
+                           MPRIS_PLAYER_INTERFACE, // interface to call on
                            dbus_method); // method name
 
     if (NULL == pending) {
