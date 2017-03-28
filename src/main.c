95f45350d473929fe05e93fccedc8b48d715f684
Marius Orcsik <marius@habarnam.ro>
2017-03-28 17:32:57 +0200

    Adding a noobish str_replace
    Adding basic information to the mpris-ctl info command

--- src/main.c
+++ src/main.c
@@ -9,6 +9,9 @@
 #include <string.h>
 
 #include "sdbus.h"
+#include "sstring.h"
+
+#define char_size       sizeof(char*)
 
 #define ARG_HELP        "help"
 #define ARG_PLAY        "play"
@@ -33,6 +36,10 @@
 #define ARG_INFO_SHUFFLE_MODE    "%shuffle"
 #define ARG_INFO_VOLUME          "%volume"
 #define ARG_INFO_LOOP_STATUS     "%loop_status"
+#define ARG_INFO_POSITION        "%position"
+
+#define TRUE_LABEL      "true"
+#define FALSE_LABEL     "false"
 
 #define HELP_MESSAGE    "MPRIS control, version %s\n" \
 "Usage:\n  %s COMMAND - Control running MPRIS player\n" \
@@ -42,7 +49,7 @@
 "\t" ARG_PAUSE "\t\tPause the player\n" \
 "\t" ARG_STOP "\t\tStop the player\n" \
 "\t" ARG_NEXT "\t\tChange track to the next in the playlist\n" \
-"\t" ARG_PREVIOUS "\t\tChange track to the previous in the playlist\n"
+"\t" ARG_PREVIOUS "\t\tChange track to the previous in the playlist" \
 "\t" ARG_STATUS "\t\tGet the play status\n" \
 "\t" ARG_INFO "\t\t[format] Get the play status - default format \"" ARG_INFO_DEFAULT_STATUS "\"\n" \
 ""
@@ -90,7 +97,7 @@ const char* get_dbus_method (char* command)
     if (strcmp(command, ARG_PLAY_PAUSE) == 0) {
         return MPRIS_METHOD_PLAY_PAUSE;
     }
-    if (strcmp(command, ARG_STATUS) == 0) {
+    if (strcmp(command, ARG_STATUS) == 0 || strcmp(command, ARG_INFO) == 0) {
         return DBUS_PROPERTIES_INTERFACE;
     }
 
@@ -107,6 +114,26 @@ void print_help(char* name)
     fprintf(stdout, help_msg, version, name);
 }
 
+void print_mpris_info(mpris_properties *props, char* format)
+{
+
+    const char* shuffle_label = (props->shuffle ? TRUE_LABEL : FALSE_LABEL);
+    char* volume_label = get_zero_string(char_size * 4);
+    snprintf(volume_label, 4, "%.2f", props->volume);
+    char* pos_label = get_zero_string(char_size * 30);
+    snprintf(pos_label, 30, "%f", (float)props->position);
+
+    format = str_replace(format, ARG_INFO_SHUFFLE_MODE, shuffle_label);
+    format = str_replace(format, ARG_INFO_PLAYBACK_STATUS, props->playback_status);
+    format = str_replace(format, ARG_INFO_VOLUME, volume_label);
+    format = str_replace(format, ARG_INFO_LOOP_STATUS, props->loop_status);
+    format = str_replace(format, ARG_INFO_POSITION, pos_label);
+    //fprintf(stderr, "%s: len %u\n", format, (unsigned)strlen(format));
+
+    fprintf(stdout, "%s\n", format);
+    free(format);
+}
+
 int main(int argc, char** argv)
 {
     char* name = argv[0];
@@ -118,10 +145,21 @@ int main(int argc, char** argv)
     if (strcmp(command, ARG_HELP) == 0) {
         goto _help;
     }
+    char *info_format;
+    if (strcmp(command, ARG_INFO) == 0) {
+        if (argc > 2) {
+            info_format = argv[2];
+        } else {
+            info_format = ARG_INFO_DEFAULT_STATUS;
+        }
+    }
+    if (strcmp(command, ARG_STATUS) == 0) {
+        info_format = ARG_INFO_PLAYBACK_STATUS;
+    }
 
     char *dbus_method = (char*)get_dbus_method(command);
     if (NULL == dbus_method) {
-        fprintf(stderr, "Invalid command %s (use help for help)\n", command);
+        //fprintf(stderr, "Invalid command %s (use help for help)\n", command);
         goto _error;
     }
     char *dbus_property = NULL;
@@ -136,7 +174,7 @@ int main(int argc, char** argv)
     // connect to the system bus and check for errors
     conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
     if (dbus_error_is_set(&err)) {
-        fprintf(stderr, "Connection error(%s)\n", err.message);
+        //fprintf(stderr, "Connection error(%s)\n", err.message);
         dbus_error_free(&err);
     }
     if (NULL == conn) {
@@ -164,8 +202,8 @@ int main(int argc, char** argv)
                          MPRIS_PLAYER_INTERFACE,
                          dbus_method);
     } else {
-        mpris_properties prop = get_mpris_properties(conn, destination);
-        fprintf(stdout, "%s\n", prop.playback_status);
+        mpris_properties properties = get_mpris_properties(conn, destination);
+        print_mpris_info(&properties, info_format);
     }
 
     dbus_connection_flush(conn);
