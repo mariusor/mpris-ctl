5823d0ad470c31a2377a8f1422805c83a75be946
Marius Orcsik <marius@habarnam.ro>
2019-12-18 14:22:28 +0100

    Renamed ARG_X constants to CMD_X

--- src/main.c
+++ src/main.c
@@ -12,52 +12,52 @@
 #include "sstring.h"
 #include "sdbus.h"
 
-#define ARG_HELP        "help"
-#define ARG_PLAY        "play"
-#define ARG_PAUSE       "pause"
-#define ARG_STOP        "stop"
-#define ARG_NEXT        "next"
-#define ARG_PREVIOUS    "prev"
-#define ARG_PLAY_PAUSE  "pp"
-#define ARG_STATUS      "status"
-#define ARG_INFO        "info"
+#define CMD_HELP        "help"
+#define CMD_PLAY        "play"
+#define CMD_PAUSE       "pause"
+#define CMD_STOP        "stop"
+#define CMD_NEXT        "next"
+#define CMD_PREVIOUS    "prev"
+#define CMD_PLAY_PAUSE  "pp"
+#define CMD_STATUS      "status"
+#define CMD_INFO        "info"
 
-#define ARG_INFO_DEFAULT_STATUS "%track_name - %album_name - %artist_name"
-#define ARG_INFO_FULL_STATUS    "Player name:\t" ARG_INFO_PLAYER_NAME "\n" \
-"Play status:\t" ARG_INFO_PLAYBACK_STATUS "\n" \
-"Track:\t\t" ARG_INFO_TRACK_NAME "\n" \
-"Artist:\t\t" ARG_INFO_ARTIST_NAME "\n" \
-"Album:\t\t" ARG_INFO_ALBUM_NAME "\n" \
-"Album Artist:\t" ARG_INFO_ALBUM_ARTIST "\n" \
-"Art URL:\t" ARG_INFO_ART_URL "\n" \
-"Track:\t\t" ARG_INFO_TRACK_NUMBER "\n" \
-"Length:\t\t" ARG_INFO_TRACK_LENGTH "\n" \
-"Volume:\t\t" ARG_INFO_VOLUME "\n" \
-"Loop status:\t" ARG_INFO_LOOP_STATUS "\n" \
-"Shuffle:\t" ARG_INFO_SHUFFLE_MODE "\n" \
-"Position:\t" ARG_INFO_POSITION "\n" \
-"Bitrate:\t" ARG_INFO_BITRATE "\n" \
-"Comment:\t" ARG_INFO_COMMENT \
+#define INFO_DEFAULT_STATUS "%track_name - %album_name - %artist_name"
+#define INFO_FULL_STATUS    "Player name:\t" INFO_PLAYER_NAME "\n" \
+"Play status:\t" INFO_PLAYBACK_STATUS "\n" \
+"Track:\t\t" INFO_TRACK_NAME "\n" \
+"Artist:\t\t" INFO_ARTIST_NAME "\n" \
+"Album:\t\t" INFO_ALBUM_NAME "\n" \
+"Album Artist:\t" INFO_ALBUM_ARTIST "\n" \
+"Art URL:\t" INFO_ART_URL "\n" \
+"Track:\t\t" INFO_TRACK_NUMBER "\n" \
+"Length:\t\t" INFO_TRACK_LENGTH "\n" \
+"Volume:\t\t" INFO_VOLUME "\n" \
+"Loop status:\t" INFO_LOOP_STATUS "\n" \
+"Shuffle:\t" INFO_SHUFFLE_MODE "\n" \
+"Position:\t" INFO_POSITION "\n" \
+"Bitrate:\t" INFO_BITRATE "\n" \
+"Comment:\t" INFO_COMMENT \
 ""
 
-#define ARG_INFO_PLAYER_NAME     "%player_name"
-#define ARG_INFO_TRACK_NAME      "%track_name"
-#define ARG_INFO_TRACK_NUMBER    "%track_number"
-#define ARG_INFO_TRACK_LENGTH    "%track_length"
-#define ARG_INFO_ARTIST_NAME     "%artist_name"
-#define ARG_INFO_ALBUM_NAME      "%album_name"
-#define ARG_INFO_ALBUM_ARTIST    "%album_artist"
-#define ARG_INFO_ART_URL         "%art_url"
-#define ARG_INFO_BITRATE         "%bitrate"
-#define ARG_INFO_COMMENT         "%comment"
+#define INFO_PLAYER_NAME     "%player_name"
+#define INFO_TRACK_NAME      "%track_name"
+#define INFO_TRACK_NUMBER    "%track_number"
+#define INFO_TRACK_LENGTH    "%track_length"
+#define INFO_ARTIST_NAME     "%artist_name"
+#define INFO_ALBUM_NAME      "%album_name"
+#define INFO_ALBUM_ARTIST    "%album_artist"
+#define INFO_ART_URL         "%art_url"
+#define INFO_BITRATE         "%bitrate"
+#define INFO_COMMENT         "%comment"
 
-#define ARG_INFO_PLAYBACK_STATUS "%play_status"
-#define ARG_INFO_SHUFFLE_MODE    "%shuffle"
-#define ARG_INFO_VOLUME          "%volume"
-#define ARG_INFO_LOOP_STATUS     "%loop_status"
-#define ARG_INFO_POSITION        "%position"
+#define INFO_PLAYBACK_STATUS "%play_status"
+#define INFO_SHUFFLE_MODE    "%shuffle"
+#define INFO_VOLUME          "%volume"
+#define INFO_LOOP_STATUS     "%loop_status"
+#define INFO_POSITION        "%position"
 
-#define ARG_INFO_FULL            "%full"
+#define INFO_FULL            "%full"
 
 #define TRUE_LABEL      "true"
 #define FALSE_LABEL     "false"
@@ -65,34 +65,34 @@
 #define HELP_MESSAGE    "MPRIS control, version %s\n" \
 "Usage:\n  %s COMMAND - Control running MPRIS player\n" \
 "Commands:\n"\
-"\t" ARG_HELP "\t\tThis help message\n" \
-"\t" ARG_PLAY "\t\tBegin playing\n" \
-"\t" ARG_PLAY_PAUSE "\t\tToggle play or pause\n" \
-"\t" ARG_PAUSE "\t\tPause the player\n" \
-"\t" ARG_STOP "\t\tStop the player\n" \
-"\t" ARG_NEXT "\t\tChange track to the next in the playlist\n" \
-"\t" ARG_PREVIOUS "\t\tChange track to the previous in the playlist\n" \
-"\t" ARG_STATUS "\t\tGet the playback status\n" \
-"\t\t\t- equivalent to " ARG_INFO " \"%s\"\n" \
-"\t" ARG_INFO "\t\t<format> Display information about the current track\n" \
+"\t" CMD_HELP "\t\tThis help message\n" \
+"\t" CMD_PLAY "\t\tBegin playing\n" \
+"\t" CMD_PLAY_PAUSE "\t\tToggle play or pause\n" \
+"\t" CMD_PAUSE "\t\tPause the player\n" \
+"\t" CMD_STOP "\t\tStop the player\n" \
+"\t" CMD_NEXT "\t\tChange track to the next in the playlist\n" \
+"\t" CMD_PREVIOUS "\t\tChange track to the previous in the playlist\n" \
+"\t" CMD_STATUS "\t\tGet the playback status\n" \
+"\t\t\t- equivalent to " CMD_INFO " \"%s\"\n" \
+"\t" CMD_INFO "\t\t<format> Display information about the current track\n" \
 "\t\t\t- default value\"%s\"\n\n" \
 "Format specifiers:\n" \
-"\t%" ARG_INFO_PLAYER_NAME "\tprints the player name\n" \
-"\t%" ARG_INFO_TRACK_NAME "\tprints the track name\n" \
-"\t%" ARG_INFO_TRACK_NUMBER "\tprints the track number\n" \
-"\t%" ARG_INFO_TRACK_LENGTH "\tprints the track length (useconds)\n" \
-"\t%" ARG_INFO_ARTIST_NAME "\tprints the artist name\n" \
-"\t%" ARG_INFO_ALBUM_NAME "\tprints the album name\n" \
-"\t%" ARG_INFO_ALBUM_ARTIST "\tprints the album artist\n" \
-"\t%" ARG_INFO_ART_URL "\tprints the URL of the cover art image\n" \
-"\t%" ARG_INFO_PLAYBACK_STATUS "\tprints the playback status\n" \
-"\t%" ARG_INFO_SHUFFLE_MODE "\tprints the shuffle mode\n" \
-"\t%" ARG_INFO_VOLUME "\t\tprints the volume\n" \
-"\t%" ARG_INFO_LOOP_STATUS "\tprints the loop status\n" \
-"\t%" ARG_INFO_POSITION "\tprints the song position (useconds)\n" \
-"\t%" ARG_INFO_BITRATE "\tprints the track's bitrate\n" \
-"\t%" ARG_INFO_COMMENT "\tprints the track's comment\n" \
-"\t%" ARG_INFO_FULL "\t\tprints all available information\n" \
+"\t%" INFO_PLAYER_NAME "\tprints the player name\n" \
+"\t%" INFO_TRACK_NAME "\tprints the track name\n" \
+"\t%" INFO_TRACK_NUMBER "\tprints the track number\n" \
+"\t%" INFO_TRACK_LENGTH "\tprints the track length (useconds)\n" \
+"\t%" INFO_ARTIST_NAME "\tprints the artist name\n" \
+"\t%" INFO_ALBUM_NAME "\tprints the album name\n" \
+"\t%" INFO_ALBUM_ARTIST "\tprints the album artist\n" \
+"\t%" INFO_ART_URL "\tprints the URL of the cover art image\n" \
+"\t%" INFO_PLAYBACK_STATUS "\tprints the playback status\n" \
+"\t%" INFO_SHUFFLE_MODE "\tprints the shuffle mode\n" \
+"\t%" INFO_VOLUME "\t\tprints the volume\n" \
+"\t%" INFO_LOOP_STATUS "\tprints the loop status\n" \
+"\t%" INFO_POSITION "\tprints the song position (useconds)\n" \
+"\t%" INFO_BITRATE "\tprints the track's bitrate\n" \
+"\t%" INFO_COMMENT "\tprints the track's comment\n" \
+"\t%" INFO_FULL "\t\tprints all available information\n" \
 ""
 
 const char* get_version()
@@ -106,10 +106,10 @@ const char* get_version()
 const char* get_dbus_property_name (char* command)
 {
     if (NULL == command) return NULL;
-    if (strcmp(command, ARG_STATUS) == 0) {
+    if (strcmp(command, CMD_STATUS) == 0) {
         return MPRIS_PROP_PLAYBACK_STATUS;
     }
-    if (strcmp(command, ARG_INFO) == 0) {
+    if (strcmp(command, CMD_INFO) == 0) {
         return MPRIS_PROP_METADATA;
     }
 
@@ -120,25 +120,25 @@ const char* get_dbus_method (char* command)
 {
     if (NULL == command) return NULL;
 
-    if (strcmp(command, ARG_PLAY) == 0) {
+    if (strcmp(command, CMD_PLAY) == 0) {
         return MPRIS_METHOD_PLAY;
     }
-    if (strcmp(command,ARG_PAUSE) == 0) {
+    if (strcmp(command,CMD_PAUSE) == 0) {
         return MPRIS_METHOD_PAUSE;
     }
-    if (strcmp(command, ARG_STOP) == 0) {
+    if (strcmp(command, CMD_STOP) == 0) {
         return MPRIS_METHOD_STOP;
     }
-    if (strcmp(command, ARG_NEXT) == 0) {
+    if (strcmp(command, CMD_NEXT) == 0) {
         return MPRIS_METHOD_NEXT;
     }
-    if (strcmp(command, ARG_PREVIOUS) == 0) {
+    if (strcmp(command, CMD_PREVIOUS) == 0) {
         return MPRIS_METHOD_PREVIOUS;
     }
-    if (strcmp(command, ARG_PLAY_PAUSE) == 0) {
+    if (strcmp(command, CMD_PLAY_PAUSE) == 0) {
         return MPRIS_METHOD_PLAY_PAUSE;
     }
-    if (strcmp(command, ARG_STATUS) == 0 || strcmp(command, ARG_INFO) == 0) {
+    if (strcmp(command, CMD_STATUS) == 0 || strcmp(command, CMD_INFO) == 0) {
         return DBUS_PROPERTIES_INTERFACE;
     }
 
@@ -151,15 +151,15 @@ void print_help(char* name)
     const char* version = get_version();
 
     help_msg = HELP_MESSAGE;
-    char* info_def = ARG_INFO_DEFAULT_STATUS;
-    char* status_def = ARG_INFO_PLAYBACK_STATUS;
+    char* info_def = INFO_DEFAULT_STATUS;
+    char* status_def = INFO_PLAYBACK_STATUS;
 
     fprintf(stdout, help_msg, version, name, status_def, info_def);
 }
 
 void print_mpris_info(mpris_properties *props, char* format)
 {
-    const char* info_full = ARG_INFO_FULL_STATUS;
+    const char* info_full = INFO_FULL_STATUS;
     const char* shuffle_label = (props->shuffle ? TRUE_LABEL : FALSE_LABEL);
     char* volume_label = get_zero_string(4);
     if (NULL == volume_label) { return; }
@@ -184,23 +184,23 @@ void print_mpris_info(mpris_properties *props, char* format)
     output = str_replace(output, "\\n", "\n");
     output = str_replace(output, "\\t", "\t");
 
-    output = str_replace(output, ARG_INFO_FULL, info_full);
+    output = str_replace(output, INFO_FULL, info_full);
 
-    output = str_replace(output, ARG_INFO_PLAYER_NAME, props->player_name);
-    output = str_replace(output, ARG_INFO_SHUFFLE_MODE, shuffle_label);
-    output = str_replace(output, ARG_INFO_PLAYBACK_STATUS, props->playback_status);
-    output = str_replace(output, ARG_INFO_VOLUME, volume_label);
-    output = str_replace(output, ARG_INFO_LOOP_STATUS, props->loop_status);
-    output = str_replace(output, ARG_INFO_POSITION, pos_label);
-    output = str_replace(output, ARG_INFO_TRACK_NAME, props->metadata.title);
-    output = str_replace(output, ARG_INFO_ARTIST_NAME, props->metadata.artist);
-    output = str_replace(output, ARG_INFO_ALBUM_ARTIST, props->metadata.album_artist);
-    output = str_replace(output, ARG_INFO_ALBUM_NAME, props->metadata.album);
-    output = str_replace(output, ARG_INFO_TRACK_LENGTH, length_label);
-    output = str_replace(output, ARG_INFO_TRACK_NUMBER, track_number_label);
-    output = str_replace(output, ARG_INFO_BITRATE, bitrate_label);
-    output = str_replace(output, ARG_INFO_COMMENT, props->metadata.comment);
-    output = str_replace(output, ARG_INFO_ART_URL, props->metadata.art_url);
+    output = str_replace(output, INFO_PLAYER_NAME, props->player_name);
+    output = str_replace(output, INFO_SHUFFLE_MODE, shuffle_label);
+    output = str_replace(output, INFO_PLAYBACK_STATUS, props->playback_status);
+    output = str_replace(output, INFO_VOLUME, volume_label);
+    output = str_replace(output, INFO_LOOP_STATUS, props->loop_status);
+    output = str_replace(output, INFO_POSITION, pos_label);
+    output = str_replace(output, INFO_TRACK_NAME, props->metadata.title);
+    output = str_replace(output, INFO_ARTIST_NAME, props->metadata.artist);
+    output = str_replace(output, INFO_ALBUM_ARTIST, props->metadata.album_artist);
+    output = str_replace(output, INFO_ALBUM_NAME, props->metadata.album);
+    output = str_replace(output, INFO_TRACK_LENGTH, length_label);
+    output = str_replace(output, INFO_TRACK_NUMBER, track_number_label);
+    output = str_replace(output, INFO_BITRATE, bitrate_label);
+    output = str_replace(output, INFO_COMMENT, props->metadata.comment);
+    output = str_replace(output, INFO_ART_URL, props->metadata.art_url);
 
     fprintf(stdout, "%s\n", output);
     free(output);
@@ -224,15 +224,15 @@ int main(int argc, char** argv)
     }
 
     char *command = argv[1];
-    if (strcmp(command, ARG_HELP) == 0) {
+    if (strcmp(command, CMD_HELP) == 0) {
         goto _help;
     }
-    char *info_format = ARG_INFO_DEFAULT_STATUS;
-    if (strcmp(command, ARG_INFO) == 0 && argc > 2) {
+    char *info_format = INFO_DEFAULT_STATUS;
+    if (strcmp(command, CMD_INFO) == 0 && argc > 2) {
         info_format = argv[2];
     }
-    if (strcmp(command, ARG_STATUS) == 0) {
-        info_format = ARG_INFO_PLAYBACK_STATUS;
+    if (strcmp(command, CMD_STATUS) == 0) {
+        info_format = INFO_PLAYBACK_STATUS;
     }
 
     char *dbus_method = (char*)get_dbus_method(command);
